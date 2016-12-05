e3b6156051e14e768f2063cd358547eacb21ddd1
heinrichquirit <heinrichquirit@gmail.com>
2016-12-05 15:38:36 +1100

    commit change

--- GitTest2Project/ViewController.swift
+++ GitTest2Project/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Make changes for git")
+        print("Make another change for git")
     }
 
     override func didReceiveMemoryWarning() {
